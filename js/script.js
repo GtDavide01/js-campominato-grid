103467400559a0cb31a5efb70b31b20e0fad7c05
GtDavide01 <davidefiorini01@gmail.com>
2022-10-17 15:45:12 +0200

    problema griglia in loop

--- js/script.js
+++ js/script.js
@@ -19,6 +19,54 @@
 //recupero il bottone in html
 const btnPlay = document.getElementById("play");
 console.log(btnPlay);
+//recupero il wrapper che contiene la griglia di gioco 
+const wrapperGame = document.querySelector(".wrapperbox");
+console.log(wrapperGame);
+//recupero row che conterra il numero di caselle di gioco
+const rowGame = document.querySelector(".row");
+console.log(rowGame);
+//recupero i box della griglia 
+const boxGame = document.querySelectorAll(".box");
+console.log(boxGame);
+//quando clicco il bottone compare la griglia
+btnPlay.addEventListener("click" , function(){
+    wrapperGame.classList.remove("none");
+    wrapperGame.classList.add("active");
+    //variabile contenente numero di caselle per gioco 
+    const gameBoxes = 100;
+    //creo array che conterra le 100 caselle
+    const arrayGame = [];
+    while(arrayGame.length < gameBoxes){
+        //variabile con numeri random 
+        const rndNumberArray = randomNuber(1, gameBoxes);
+        //Se l'array  non include il numero allora pusho altrimenti continua con la generazione di numeri 
+        if (!arrayGame.includes(rndNumberArray)){
+            arrayGame.push(rndNumberArray);
+        }
+    }
+    console.log(arrayGame);
+    //per ogni elemento dell'array creo una classe box 
+    for ( let i=0 ; i<arrayGame.length ; i++ ){
+        //creo elemento div 
+        const addBox = document.createElement("div");
+        //lo collego alla row
+        rowGame.append(addBox);
+        //aggiungo la classe css box
+        addBox.classList.add("box");
+        //per ogni elemento stampo anche il numeor 
+        addBox.innerHTML = arrayGame[i];
+        //se clicclo sulla cella stampo numero della casella e la coloro di azzurro 
+        addBox.addEventListener("click",function(){
+            console.log(arrayGame[i]);
+            addBox.classList.add("color");
+        })
+    }
+    })
+
+
+
+
+
 
 
 
